e7690f9cad89497c7af7d95c0346cd1d76c0cba9
sowmyareddy1234 <sowmyareddy45776@gmail.com>
2023-04-05 11:59:24 +0530

    final edit

--- todolist_app/views.py
+++ todolist_app/views.py
@@ -20,7 +20,7 @@ def todolist(request):
         return redirect('todolist')
     else:
         all_tasks= Tasklist.objects.filter(manage=request.user)
-        paginator= Paginator(all_tasks,15)
+        paginator= Paginator(all_tasks,4)
         page=request.GET.get('pg')
         all_tasks=paginator.get_page(page)
         return render(request,'todolist.html',{'all_tasks': all_tasks})
