f2c950c7cecdb1b08cf9691d58e41c95717440f8
sowmyareddy1234 <sowmyareddy45776@gmail.com>
2023-04-09 12:23:38 +0530

    forth commit

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DJANGO_DEBUG')
 
-ALLOWED_HOSTS= [env('DJANGO_ALLOWED_HOST')]
+#ALLOWED_HOSTS= [env('DJANGO_ALLOWED_HOST')]
 
 #ALLOWED_HOSTS = ['localhost', '127.0.0.1','taskmate-production.up.railway.app']
 #CSRF_TRUSTED_ORIGINS = ['https://taskmate-production.up.railway.app']
